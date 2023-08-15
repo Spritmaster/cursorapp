22273e3a0a395b357e4e703d6fda7416e96b16c7
swordmaster2005 <soulmaster9794@gmail.com>
2023-08-15 08:19:46 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+         <p>
+          All Right Reserved.{" "}
+          <a href="https://t.me/Ergashev48_09">Ergashov Nodirbek</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
